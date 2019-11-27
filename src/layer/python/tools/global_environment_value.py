45866d35d6d33f10c72ce9a546d72f9cb310827c
yuta-natsume <natsume.yuta@classmethod.jp>
2019-11-27 22:06:29 +0900

    fix save_lambda_request_id

--- src/layer/python/tools/global_environment_value.py
+++ src/layer/python/tools/global_environment_value.py
@@ -1,7 +1,19 @@
 import os
 
 
-def get_env() -> str:
+def get_custom_environment_values() -> dict:
+    result = {}
+    for key in [
+        'AWS_ENV',  # 環境の識別子。prodとかdevとか。
+        'ARTICLES_TABLE_NAME',  # 記事情報を蓄積するDynamoDB Tableの名前
+        'KMS_KEY_ID',  # PostedConfigの暗号化に使うKMSの鍵
+        'BOOKMARKS_USER_ID',  # ユーザーID (シングルユーザーの個人用だから必要ないと言えばないんだけど一応)
+    ]:
+        result[key] = os.environ.get(key)
+    return result
+
+
+def get_environ() -> str:
     return os.environ["AWS_ENV"]
 
 
