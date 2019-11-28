6ce6e63668b459cccb06d5dc6c0e257464300504
yuta-natsume <natsume.yuta@classmethod.jp>
2019-11-28 20:38:09 +0900

    fix auth

--- src/layer/python/tools/auth.py
+++ src/layer/python/tools/auth.py
@@ -1,6 +1,8 @@
-def can_access(event, ssm_client, is_header=False):
+def can_access(event, ssm_client, is_header=False) -> bool:
     store = event["headers"] if is_header else event["queryStringParameters"]
-    request_token = store["Authorization"]
+    request_token = store.get("Authorization")
+    if request_token is None:
+        return False
 
     resp = ssm_client.get_parameter(Name="BookmarksPassword", WithDecryption=True)
 
