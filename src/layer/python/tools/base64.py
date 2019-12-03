5506f6065a2a97934da09d165c347eb421d4cb46
yuta-natsume <natsume.yuta@classmethod.jp>
2019-12-03 20:01:34 +0900

    fix urlsafe base64 type

--- src/layer/python/tools/base64.py
+++ src/layer/python/tools/base64.py
@@ -1,12 +1,12 @@
 from base64 import b64decode, b64encode
 
 
-def urlsafe_encode(obj: str) -> str:
-    encoded: str = b64encode(obj.encode()).decode()
+def urlsafe_encode(obj: bytes) -> str:
+    encoded: str = b64encode(obj).decode()
     return encoded.replace("+", "-").replace("=", "_").replace("/", "~")
 
 
-def urlsafe_decode(obj: str) -> str:
+def urlsafe_decode(obj: str) -> bytes:
     unsafe = obj.replace("-", "+").replace("_", "=").replace("~", "/")
-    decoded = b64decode(unsafe.encode()).decode()
+    decoded = b64decode(unsafe.encode())
     return decoded
